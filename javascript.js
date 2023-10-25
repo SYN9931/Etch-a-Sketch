d77aa29e14b5d3f49e34fe5751d98bf200236423
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2023-10-26 06:19:08 +0800

    created a square div instead of a square shape

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-ctx.rect (20,20,80,80);
-ctx.fill();
+ctx.fillRect (20,20,80,80);
+ctx.rect (102,20,80,80);
+ctx.rect (184,20,80,80);
+ctx.rect (266,20,80,80);
+// tx.fill();
+
+
+var squareDiv = document.createElement('div');
+squareDiv.className = 'squareDiv';
+squareDiv.style.color ='red';
+document.body.appendChild(squareDiv);
