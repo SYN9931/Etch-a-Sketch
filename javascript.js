36397fdb2e49d95a253162cd3eb1ef966aad7070
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2023-10-29 05:52:36 +0800

    create squares within the main div
    added for loop to experiment
    next step is to arrange the divs to display in grid pattern

--- javascript.js
+++ javascript.js
@@ -1,13 +1,6 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-ctx.fillRect (20,20,80,80);
-ctx.rect (102,20,80,80);
-ctx.rect (184,20,80,80);
-ctx.rect (266,20,80,80);
-// tx.fill();
-
-
-var squareDiv = document.createElement('div');
-squareDiv.className = 'squareDiv';
-squareDiv.style.color ='red';
-document.body.appendChild(squareDiv);
+for(let i=0; i<17; i++){
+    var div = document.createElement("div");
+    div.setAttribute('class','squareUnit');
+    document.getElementById("mainDiv").appendChild(div);
+    
+}
