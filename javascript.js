4c86f60e440e7b381c35dda3530f8423a210f03f
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2023-10-30 06:08:58 +0800

    removed previous code new code utilises css display grid method

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
-for(let i=0; i<17; i++){
-    var div = document.createElement("div");
-    div.setAttribute('class','squareUnit');
-    document.getElementById("mainDiv").appendChild(div);
-    
-}
+const container = document.getElementById("container");
+
+function makeRows (rows, cols){
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols',cols);
+    for(c=0;c<(rows*cols); c++){
+        let cell = document.createElement("div");
+        //cell.innerText = (c+1);
+        container.appendChild(cell).className = "grid-item";
+
+    };
+};
+
+makeRows(16,16);
