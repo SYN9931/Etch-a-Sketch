53e9c00f3278cc61dc7bff25b0bd5f3e2187cd83
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2023-11-16 06:26:15 +0800

    created main grid inside of the main container added background color to the main grid

--- javascript.js
+++ javascript.js
@@ -4,4 +4,16 @@ function createDiv(){
     document.body.appendChild(mainDiv);
     mainDiv.setAttribute("id","mainDiv");
 }
+
+//initialize the main container
 createDiv();
+
+//grid container
+function createGrid(){
+    const mainGrid = document.createElement("div");
+    mainDiv.appendChild(mainGrid);
+    mainGrid.setAttribute("id", "mainGrid");
+}
+
+//initialize the grid container
+createGrid();
