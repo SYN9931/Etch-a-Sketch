778bb990eb72b14a912615da683f24e1511e3535
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2023-11-16 06:00:40 +0800

    removed all previous code since tutorial ask specifically to use flexbox created main container thorugh DOM method

--- javascript.js
+++ javascript.js
@@ -1,27 +1,7 @@
-const container = document.getElementById("container");
-
-function makeRows (rows, cols){
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols',cols);
-    for(c=0;c<(rows*cols); c++){
-        let cell = document.createElement("div");
-        //cell.innerText = (c+1);
-        container.appendChild(cell).className = "grid-item";
-
-    };
-};
-
-makeRows(10,10);
-function background(){
-    const clr1  ='rgb(65,224,208)';
-    return clr1;
-
+//create main container through DOM method
+function createDiv(){
+    const mainDiv = document.createElement("div");
+    document.body.appendChild(mainDiv);
+    mainDiv.setAttribute("id","mainDiv");
 }
-
-let divs = document.querySelectorAll('div');
-for(let i=0; i<divs.length;i++){
-    divs[i].onmouseover=function(e){
-        e.target.style.backgroundColor=background();
-    }
-
-}
+createDiv();
